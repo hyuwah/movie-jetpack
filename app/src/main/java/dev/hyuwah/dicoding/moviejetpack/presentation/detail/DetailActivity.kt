79738ecc5f0bdb9b51031e60adf2c057602fa8a7
Muhammad Wahyudin <muhammad.whydn@gmail.com>
2019-11-22 10:14:15 +0700

    Fix minor issue Detail Activity & cleanup

--- app/src/main/java/dev/hyuwah/dicoding/moviejetpack/presentation/detail/DetailActivity.kt
+++ app/src/main/java/dev/hyuwah/dicoding/moviejetpack/presentation/detail/DetailActivity.kt
@@ -3,6 +3,7 @@ package dev.hyuwah.dicoding.moviejetpack.presentation.detail
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import dev.hyuwah.dicoding.moviejetpack.R
+import dev.hyuwah.dicoding.moviejetpack.data.Constant.TYPES
 import dev.hyuwah.dicoding.moviejetpack.data.MovieModel
 import kotlinx.android.synthetic.main.activity_detail.*
 import kotlinx.android.synthetic.main.view_detail_description.*
@@ -15,6 +16,7 @@ class DetailActivity : AppCompatActivity() {
         const val ID = "id"
         const val TYPE = "type"
     }
+
 
     private val viewModel: DetailViewModel by viewModel()
     private lateinit var type: String
@@ -30,12 +32,13 @@ class DetailActivity : AppCompatActivity() {
         val type = intent.getStringExtra(TYPE)
 
         type?.let {
-            tv_detail_title.text = type
-            if (type == "movie") {
+            if (type == TYPES.MOVIE.name) {
+                tv_detail_title.text = TYPES.MOVIE.value
                 viewModel.getMovieById(id)?.let { movie ->
                     setupView(movie)
                 }
             } else {
+                tv_detail_title.text = TYPES.TVSHOW.value
                 viewModel.getTvShowById(id)?.let { tvShow ->
                     setupView(tvShow)
                 }
@@ -44,6 +47,7 @@ class DetailActivity : AppCompatActivity() {
     }
 
     private fun setupView(movieItem: MovieModel) {
+        supportActionBar?.title = movieItem.title
         iv_detail_poster.setImageResource(movieItem.posterPath)
         tv_detail_genre.text = String.format(getString(R.string.count_voters), movieItem.voteCount)
         tv_detail_runtime.text = movieItem.releaseDate
